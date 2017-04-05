c47b42df217dc06b6de1b2bd8a9df725a4d14305
Alexander Li <alexander.li@ef.com>
2017-04-05 09:28:59 +0800

    add working no_disturbing msg

--- no_distrubing.py
+++ no_distrubing.py
@@ -1,7 +1,8 @@
 from wxpy import *
 bot = Bot()
 # no_disturbing_str = '嘉嘉呼呼中，有急事给我电话哈，做个好梦[Joyful]\nAlexander is asleep, please call him if there is an emergency, have a nice dream Dear[Joyful]'
-no_disturbing_str = '嘉嘉去运动了，有急事的话给我电话哈，[Joyful]\nAlexander is out for exercise, please call me if there is an emergency, Carpe Diem.[Joyful]'
+# no_disturbing_str = '嘉嘉去运动了，有急事的话给我电话哈，[Joyful]\nAlexander is out for exercise, please call me if there is an emergency, Carpe Diem.[Joyful]'
+no_disturbing_str = '工作中，稍后回复，有急事请电话联系^_^\nWorking time, catch you later, ping me if there is an emergency.^_^Carpe Diem.'
 
 @bot.register()
 def no_disturbing_reply(msg):
